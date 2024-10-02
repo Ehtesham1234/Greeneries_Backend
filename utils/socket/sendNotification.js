2f7ca3a591b1429aadfc8e643f312792e73d5e48
Ehtesham1234 <ehteshamusman12344321@gmail.com>
2024-10-02 14:49:25 +0530

    changes

--- utils/socket/sendNotification.js
+++ utils/socket/sendNotification.js
@@ -15,7 +15,7 @@ exports.sendPushNotification = async (fcmToken, title, body, data) => {
     return;
   }
 
-  console.log("receiver, title, body, data", fcmToken, title, body, data);
+  // console.log("receiver, title, body, data", fcmToken, title, body, data);
 
   const message = {
     token: fcmToken,
@@ -30,7 +30,7 @@ exports.sendPushNotification = async (fcmToken, title, body, data) => {
     // Include the additional data here
   };
 
-  console.log("message", message);
+  // console.log("message", message);
 
   try {
     const response = await admin.messaging().send(message);
